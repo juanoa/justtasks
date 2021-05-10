4e6b8fefb1c1527605211521f2470e2b1201218f
Juan Otálora Alarcón <juan@juanoa.com>
2021-05-10 23:10:14 +0200

    Navbar refactor

--- packages/justtasks-webapp/src/components/ui/navbar.js
+++ packages/justtasks-webapp/src/components/ui/navbar.js
@@ -1,28 +1,29 @@
 import React from 'react'
+import {Link} from "react-router-dom";
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">JustTasks</a>
+        <Link className="navbar-brand" to="/">JustTasks</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"/>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">Dashboard</a>
+              <Link className="nav-link" to="/">Dashboard</Link>
             </li>
           </ul>
           <ul className="navbar-nav">
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
+              <a className="nav-link dropdown-toggle" href="/" id="navbarDropdownMenuLink" role="button"
                  data-bs-toggle="dropdown" aria-expanded="false">
                 Juan (juan@juanoa.com)
               </a>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <li><a className="dropdown-item" href="#">Configuration</a></li>
-                <li><a className="dropdown-item text-danger" href="#">Logout</a></li>
+                <li><Link className="dropdown-item" to="/">Configuration</Link></li>
+                <li><Link className="dropdown-item text-danger" to="/">Logout</Link></li>
               </ul>
             </li>
           </ul>
