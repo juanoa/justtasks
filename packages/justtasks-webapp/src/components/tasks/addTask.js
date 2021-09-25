ddd2e35379d84303666fdb3b79f65dfc7cef1279
Juan Otálora Alarcón <juan@juanoa.com>
2021-09-25 16:02:10 +0200

    Make the AddTask input disappear when clicking out of it

--- packages/justtasks-webapp/src/components/tasks/addTask.js
+++ packages/justtasks-webapp/src/components/tasks/addTask.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {useDispatch} from "react-redux";
 
 import {useForm} from "../../hooks/useForm";
@@ -20,11 +20,35 @@ export const AddTask = ({day}) => {
   const handleShowInput = () => {
     setShowInput(true)
   }
+
   const handleHideInput = (e) => {
     if (e.key === "Escape") {
       setShowInput(false)
     }
   }
+
+  const useOutsideAlerter = (ref) => {
+    useEffect(() => {
+      /**
+       * Alert if clicked on outside of element
+       */
+      function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+          setShowInput(false)
+        }
+      }
+      // Bind the event listener
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        // Unbind the event listener on clean up
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }, [ref]);
+  }
+
+  const wrapperRef = useRef(null);
+  useOutsideAlerter(wrapperRef);
+
 
   const handleForm = (e) => {
     e.preventDefault()
@@ -37,12 +61,12 @@ export const AddTask = ({day}) => {
       {
         (showInput)
           ? (
-            <form onSubmit={handleForm}>
+            <form onSubmit={handleForm} ref={wrapperRef}>
               <div className="input-group mb-3">
                 <input
                   type="text"
                   className="form-control"
-                  placeholder="[Esc] to exit"
+                  placeholder="What's on your mind?"
                   name="titleNewTask"
                   autoFocus={true}
                   autoComplete="off"
