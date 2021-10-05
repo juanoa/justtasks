f7bd8bb38267a09cbe97196b2e14d1c64dda521b
Juan Otálora Alarcón <juan@juanoa.com>
2021-10-05 19:59:25 +0200

    Reorganized tasks respect the new day index

--- packages/justtasks-api/helpers/indexes.js
+++ packages/justtasks-api/helpers/indexes.js
@@ -1,5 +1,8 @@
 const Task = require('../models/Task')
 
+/*
+  If it's the first task, return 0; else, return the next index available
+ */
 const getNewIndexFromUserAndDate = async (uid, date) => {
   const tasks = await Task.find({user: uid, day: date})
   return(tasks.length || 0)
--- packages/justtasks-api/helpers/reorganize.js
+++ packages/justtasks-api/helpers/reorganize.js
@@ -1,11 +1,16 @@
 const Task = require('../models/Task')
 const moment = require('moment')
+const {getNewIndexFromUserAndDate} = require("./indexes");
 
 const reorganizeByUser = async (uid, clientDateString) => {
   const clientDate = moment(clientDateString).format('YYYYMMDD')
+  // Get previous user uncompleted tasks
   const tasks = await Task.find({user: uid, completed: false, day: {$lt: clientDate}})
   for (const task of tasks) {
+    // Put the client's current day
     task.day = clientDate
+    // Get new index for the new date
+    task.index = await getNewIndexFromUserAndDate(uid, clientDate)
     await Task.findByIdAndUpdate(task.id, task)
   }
 }
