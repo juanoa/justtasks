271f31ab3054a7147d0f238422bdea2426a78740
Juan Otálora <juan@juanoa.com>
2021-12-04 16:53:38 +0100

    Update initial users

--- docker/mongo-entrypoint/mongo-init.js
+++ docker/mongo-entrypoint/mongo-init.js
@@ -5,14 +5,14 @@ db.createCollection('users');
 db.users.insertMany([
   {
     name: "Premium User",
-    email: "premium@justtasks.com",
+    email: "premium@justtasks.app",
     password: "$2a$10$ahR0QErnID4rpFbIYTS3T.ZuusgmeneEtdcfUdMt0TL1XW2nlMae2",
     premium: true,
     premiumSince: "2021-01-01T01:01:01.000+00:00"
   },
   {
     name: "Basic User",
-    email: "basic@justtasks.com",
+    email: "basic@justtasks.app",
     password: "$2a$10$ahR0QErnID4rpFbIYTS3T.ZuusgmeneEtdcfUdMt0TL1XW2nlMae2",
     premium: false
   }
