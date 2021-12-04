4442738b8458c5507d8eb908ede3cb41d27014c5
Juan Otálora <juan@juanoa.com>
2021-12-04 17:10:27 +0100

    Error

--- packages/app/src/components/ui/navbar.js
+++ packages/app/src/components/ui/navbar.js
@@ -15,7 +15,7 @@ export const Navbar = () => {
   }
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark"
       <div className="container-fluid">
         <Link className="navbar-brand" to="/home">
           <img
